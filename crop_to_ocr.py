27d499a9eaf89325e59b060a63ae4ce0b90a5983
Saumya Shah <shahsaumya0108@gmail.com>
2018-07-06 16:03:53 +0530

    Updated OCR text generation script

--- crop_to_ocr.py
+++ crop_to_ocr.py
@@ -20,35 +20,64 @@ import os
 import sys
 import pytesseract
 from PIL import Image
+import re
+
 
 folder_path = sys.argv[1]
 dest_path = sys.argv[2]
 folder = os.listdir(folder_path)
 checked = {}    # all files that have already been used for OCR
 
+
+def stringSplitByNumbers(x):
+    r = re.compile('_')
+    l = r.split(x)
+    return [int(y) if y.isdigit() else y for y in l]
+
+
+folder = [file.replace('.jpg', '') for file in folder]
+folder = sorted(folder, key=stringSplitByNumbers)
+folder = [file + '.jpg' for file in folder]
+
+print(folder)
+
 print("Enter your choice for OCR method\n1. One file for each image\n2. One file for each directory\n")
 
 ch = int(input())
 
-for file in sorted(folder):
+for file in folder:
     checked[file] = 0  # initializing all files
 
 if(ch == 1):
-    for file in sorted(folder):
-        file_base = file.split('_')
+    for file in folder:
         print(file)
+        file_base = file.split('_')
+
+        if int(file_base[1].split('.')[0]) == 0:    # if there is a continuation of text to the next page
+            print("0 filename here!")
+            name = ""
+            number = str(int(file_base[0]) - 1)     # fetching previous file name
+            for i in range(0, (5 - len(number))):
+                name = "0" + number
+                number = name
+            f = open(dest_path + "/" + name + ".txt", "a+")
+            checked[file] = 1
+            text = pytesseract.image_to_string(Image.open(folder_path + "/" + file))  # OCR on image
+            f.write(text)
+            f.close()
+
         if checked[file] == 0:
             f = open(dest_path + "/" + file_base[0] + ".txt", "a+")    # open file
             text = pytesseract.image_to_string(Image.open(folder_path + "/" + file))  # OCR on image
-            f.write(text)   # write text to file
             f.write("\n--------------------\n")
+            f.write(text)   # write text to file
             checked[file] = 1
-            for i in sorted(folder):
+            for i in folder:
                 if os.path.isfile(os.path.join(folder_path + "/" + i)) and file_base[0] in i and checked[file] == 0:
                     checked[i] = 1
                     text = pytesseract.image_to_string(Image.open(folder_path + "/" + i))
-                    f.write(text)
                     f.write("\n--------------------\n")
+                    f.write(text)
             f.close()   # close file
 
         else:
@@ -56,13 +85,19 @@ if(ch == 1):
 
 elif(ch == 2):
     f = open(dest_path + "/" + folder_path.split('/')[-2] + ".txt", "a+")
-    for file in sorted(folder):
+    for file in folder:
         print(file)
+        file_base = file.split('_')
         if checked[file] == 0:
-            text = pytesseract.image_to_string(Image.open(folder_path + "/" + file))  # OCR on image
-            f.write(text)   # write text to file
-            f.write("\n--------------------\n")
-            checked[file] = 1
+            if int(file_base[1].split('.')[0]) == 0:
+                text = pytesseract.image_to_string(Image.open(folder_path + "/" + file))  # OCR on image
+                f.write(text)   # write text to file
+                checked[file] = 1
+            else:
+                text = pytesseract.image_to_string(Image.open(folder_path + "/" + file))  # OCR on image
+                f.write("\n--------------------\n")
+                f.write(text)   # write text to file
+                checked[file] = 1
 
         else:
             continue
